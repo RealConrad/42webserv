f1f815bfda82bd4ff6aa077c8a9db6f526e4f490
Conrad <wenzconrad@gmail.com>
2024-04-09 00:18:27 +0000

    ADded a HTTPResponse class

---
+++ includes/HTTPResponse.hpp
@@ -0,0 +1,24 @@
+#ifndef HTTP_RESPONSE_HPP
+# define HTTP_RESPONSE_HPP
+
+#include <map>
+#include <string>
+#include <sstream>
+#include "Logger.hpp"
+
+class HTTPResponse {
+    private:
+        int statusCode;
+        std::map<std::string, std::string> headers;
+        std::string body;
+    public:
+        HTTPResponse();
+        ~HTTPResponse();
+        void setStatusCode(int code);
+        void setHeader(const std::string& key, const std::string& value);
+        void setBody(const std::string& body);
+        std::string convertToString() const;
+
+};
+
+#endif
