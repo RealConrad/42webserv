1adcdfb39ca2354f879faa41631f2846d2ca0a9c
cwenz <wenzconrad@gmail.com>
2024-04-23 18:17:13 +0200

    added feature to specificy how many times we can print output. requested by Konrad

--- www/cgi/custom_hello.py
+++ www/cgi/custom_hello.py
@@ -9,10 +9,20 @@ if query_string:
 	pairs = query_string.split("&")
 	for pair in pairs:
 		if '=' in pair:
-			key, value = pair.split("=")
-			params[key] = value
+			parts = pair.split("=", 1)
+			if len(parts) == 2:
+				key, value = parts
+				params[key] = value
+			elif len(parts) == 1:
+				params[part[0]] = ""
 
 param_value = params.get("param", "Hello World!")
+num_value = params.get("num", "1")
 
-for i in range(5):
-	print(f"<p>{param_value}</p>")
+try:
+	num_value = int(num_value)
+except ValueError:
+	num_value = 1
+
+for i in range(num_value):
+    print(f"<p>{param_value}</p>")
